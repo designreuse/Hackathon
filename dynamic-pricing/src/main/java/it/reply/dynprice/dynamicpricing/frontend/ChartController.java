bbf10832c2c864aedee56e0ddfb1aa7f65d192f7
Rullo Alessandro <a.rullo@reply.it>
2018-05-05 13:05:07 +0200

    GSON

--- dynamic-pricing/src/main/java/it/reply/dynprice/dynamicpricing/frontend/ChartController.java
+++ dynamic-pricing/src/main/java/it/reply/dynprice/dynamicpricing/frontend/ChartController.java
@@ -4,13 +4,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
+
 @RestController
 public class ChartController {
 	
 	@RequestMapping(value = "/test", method = RequestMethod.GET, produces = "application/json")
 	public String test() {
 		
-		return "{labels:[\'January\', \'February\', \'March\', \'April\', \'May\', \'June\'],data:[66,95,46,34,88,67]}";
+		ChartData dummyChartData = new ChartData();
+		
+		Gson gson = new Gson();
+		
+		return gson.toJson(dummyChartData);
 		
 	}
 
