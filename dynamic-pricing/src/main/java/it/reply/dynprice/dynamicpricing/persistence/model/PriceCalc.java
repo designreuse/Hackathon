73406ae6c1a8acce3252ed4da7f902fef678aa36
Benjamin <b.koch@reply.de>
2018-05-05 15:00:31 +0200

    Maths stuff (PriceO, PriceODao, PriceCalc)

---
+++ dynamic-pricing/src/main/java/it/reply/dynprice/dynamicpricing/persistence/model/PriceCalc.java
@@ -0,0 +1,45 @@
+package it.reply.dynprice.dynamicpricing.persistence.model;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.reply.dynprice.dynamicpricing.persistence.dao.PriceODao;
+
+@Service
+public class PriceCalc {
+	
+	@Autowired
+	private PriceODao priceODao;
+	
+	public double calc_CostsDirAll(double qnt_prod, double costs_dir_unit) {
+		return qnt_prod * costs_dir_unit;
+	}
+	
+	public double calc_CostsOpUnit(double costs_op_to_dir, double costs_dir_unit) {
+		return costs_op_to_dir * costs_dir_unit;
+	}
+	
+	public double calc_CostsTotalUnit(double costs_dir_unit, double costs_op_unit) {
+		return costs_dir_unit + costs_op_unit;
+	}
+	
+	public double calc_Price(double costs_total_unit, double margin) {
+		return costs_total_unit * (1 + margin);
+	}
+	
+	public double calc_CostsOpToDir(double costs_op, double costs_total) {
+		return costs_op / costs_total;
+	}
+	
+	public double calc_CostsDir() {
+		double temp_sum = 0;
+		List<PriceO> priceOList = priceODao.findAll();
+		
+		for (PriceO i : priceOList) {
+			temp_sum += i.getCosts_dir_all();
+		}
+		return temp_sum;
+	}
+}
